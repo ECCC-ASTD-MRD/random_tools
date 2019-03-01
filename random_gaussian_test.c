43d23b06f4128770ccc36026aa87d76588cdfa81
M. Valin <mfvalin@gmail.com>
2019-03-01 15:56:00 -0500

    mpi version messages only from rank 0 for gaussian test

--- random_gaussian_test.c
+++ random_gaussian_test.c
@@ -49,18 +49,25 @@ int main(int argc, char **argv){
   uint32_t myseed = 123456;
   double x, p1, p2, prob, ptot;
   int est[2001];
+  int mpi_rank = 0;
+  int ierr;
   union{
     long l;
     double d;
   }v;
 // check constants
   MPI_Init(&argc,&argv);
-  v.d = INVM31 ; printf("%5i %16.16lx %24.20g\n",31,v.l,v.d);
-  v.d = INVM32 ; printf("%5i %16.16lx %24.20g\n",32,v.l,v.d);
-  v.d = INVM33 ; printf("%5i %16.16lx %24.20g\n",33,v.l,v.d);
-  v.d = INVM63 ; printf("%5i %16.16lx %24.20g\n",63,v.l,v.d);
-  v.d = INVM64 ; printf("%5i %16.16lx %24.20g\n",64,v.l,v.d);
-  v.d = INVM65 ; printf("%5i %16.16lx %24.20g\n",65,v.l,v.d);
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD,&mpi_rank);
+  if(ierr != MPI_SUCCESS) {
+    MPI_Finalize();
+    return 1;
+  }
+  v.d = INVM31 ; if(mpi_rank == 0)  printf("%5i %16.16lx %24.20g\n",31,v.l,v.d);
+  v.d = INVM32 ; if(mpi_rank == 0)  printf("%5i %16.16lx %24.20g\n",32,v.l,v.d);
+  v.d = INVM33 ; if(mpi_rank == 0)  printf("%5i %16.16lx %24.20g\n",33,v.l,v.d);
+  v.d = INVM63 ; if(mpi_rank == 0)  printf("%5i %16.16lx %24.20g\n",63,v.l,v.d);
+  v.d = INVM64 ; if(mpi_rank == 0)  printf("%5i %16.16lx %24.20g\n",64,v.l,v.d);
+  v.d = INVM65 ; if(mpi_rank == 0)  printf("%5i %16.16lx %24.20g\n",65,v.l,v.d);
 //   void  RanNormalZigSetSeed(void *stream, int *piSeed, int cSeed)  ;
 //   for(i=0 ; i<1200000 ; i++) ranbuf[i] = 0;
 //   for(i=0 ; i<1200000 ; i++) ranbuf2[i] = 0.0;
@@ -70,10 +77,10 @@ int main(int argc, char **argv){
   idmin = (unsigned long long *)&dmin;
   dmax = CVTDBL_32(maxpos) ;
   dmin = CVTDBL_32(maxneg) ;
-  printf("maxpos, maxneg transformed with CVTDBL_32  : %22.18f %22.18f , %16.16Lx, %16.16Lx\n",dmax,dmin,*idmax,*idmin);
+  if(mpi_rank == 0)  printf("maxpos, maxneg transformed with CVTDBL_32  : %22.18f %22.18f , %16.16Lx, %16.16Lx\n",dmax,dmin,*idmax,*idmin);
   dmax = CVTDBLS_32(maxpos) ;
   dmin = CVTDBLS_32(maxneg) ;
-  printf("maxpos, maxneg transformed with CVTDBLS_32 : %22.18f %22.18f , %16.16Lx, %16.16Lx\n",dmax,dmin,*idmax,*idmin);
+  if(mpi_rank == 0)  printf("maxpos, maxneg transformed with CVTDBLS_32 : %22.18f %22.18f , %16.16Lx, %16.16Lx\n",dmax,dmin,*idmax,*idmin);
 
   stream = Ran_R250_new_stream(NULL, &myseed , 1);
   RanSetSeed_gaussian_stream(stream, &myseed, 1);
@@ -101,7 +108,7 @@ int main(int argc, char **argv){
     index = 1001 + rval * 100;
     biggaussdist[index] ++;
   }
-  printf("for %d samples, min = %6.3f, max = %6.3f, avg = %10.7f\n",i,dmin,dmax,avg/i);
+  if(mpi_rank == 0)  printf("for %d samples, min = %6.3f, max = %6.3f, avg = %10.7f\n",i,dmin,dmax,avg/i);
   ptot = 0.0;
   for( i=0 ; i < 2000 ; i++){
     x = (i-1001) / 100.0 ;
@@ -113,12 +120,12 @@ int main(int argc, char **argv){
     p1 = 1000000000 * prob / 2.5066283;  // sqrt(2 * pi)
     est[i] = p1 * 1;
   }
-  printf("ptot = %g, expecting 2.5066283\n",ptot);
-//   for( i=0 ; i < 10 ; i++) printf("%9d ",gaussdist[i]);
-//   for( i=0 ; i < 2000 ; i++) printf("%9d %9d\n",i,biggaussdist[i]);
-  printf("%9s %9s %10s\n","slot","population","deviation(ppm) (center at slot 1000)");
-  for( i=991 ; i <= 1010 ; i++) printf("%9d %9d %10.0f\n",i,biggaussdist[i],1000000.0*(biggaussdist[i]-est[i])*1.0/est[i]);
-  printf("\n");
+  if(mpi_rank == 0)  printf("ptot = %g, expecting 2.5066283\n",ptot);
+//   for( i=0 ; i < 10 ; i++) if(mpi_rank == 0)  printf("%9d ",gaussdist[i]);
+//   for( i=0 ; i < 2000 ; i++) if(mpi_rank == 0)  printf("%9d %9d\n",i,biggaussdist[i]);
+  if(mpi_rank == 0)  printf("%9s %9s %10s\n","slot","population","deviation(ppm) (center at slot 1000)");
+  for( i=991 ; i <= 1010 ; i++) if(mpi_rank == 0)  printf("%9d %9d %10.0f\n",i,biggaussdist[i],1000000.0*(biggaussdist[i]-est[i])*1.0/est[i]);
+  if(mpi_rank == 0)  printf("\n");
   MPI_Barrier(MPI_COMM_WORLD);
   t0 = MPI_Wtime();
   for( i=0 ; i < 1000000000 ; i++) 
@@ -128,19 +135,19 @@ int main(int argc, char **argv){
     rval = F_DRan_gaussian_stream((struct statep *) &stream);    // time Fortran entry point (costlier)
 #endif
   t1 = MPI_Wtime();
-  printf("time for 1E+9 x 1 random DRan_gaussian_stream/R250 double value = %6.3f \n",t1-t0);  // DRan_gaussian_stream256
+  if(mpi_rank == 0)  printf("time for 1E+9 x 1 random DRan_gaussian_stream/R250 double value = %6.3f \n",t1-t0);  // DRan_gaussian_stream256
 
   t1 = 0 ; t0 = 1 ; 
   INSTRUMENT(t1 = funquick ; t0 = funcalls+1 ; )
-  INSTRUMENT(printf("quick calls in gaussian generator = %7.3f\n",t1 / t0 * 100.0);)
+  INSTRUMENT(if(mpi_rank == 0)  printf("quick calls in gaussian generator = %7.3f\n",t1 / t0 * 100.0);)
   INSTRUMENT(t1 = funtails ;)
-  INSTRUMENT(printf("tail calls in gaussian generator  = %7.3f\n",t1 / t0 * 100.0);)
+  INSTRUMENT(if(mpi_rank == 0)  printf("tail calls in gaussian generator  = %7.3f\n",t1 / t0 * 100.0);)
   INSTRUMENT(t1 = funwedge ;)
-  INSTRUMENT(printf("wedge calls in gaussian generator = %7.3f\n",t1 / t0 * 100.0);)
+  INSTRUMENT(if(mpi_rank == 0)  printf("wedge calls in gaussian generator = %7.3f\n",t1 / t0 * 100.0);)
   INSTRUMENT(t1 = funloops ;)
-  INSTRUMENT(printf("extra loops in gaussian generator = %7.3f\n",t1 / t0 * 100.0);)
+  INSTRUMENT(if(mpi_rank == 0)  printf("extra loops in gaussian generator = %7.3f\n",t1 / t0 * 100.0);)
   INSTRUMENT(t1 = funused ;)
-  INSTRUMENT(printf("uniform random values used        = %7.3f\n",t1 / t0 * 100.0);)
+  INSTRUMENT(if(mpi_rank == 0)  printf("uniform random values used        = %7.3f\n",t1 / t0 * 100.0);)
   MPI_Finalize();
   return(0);
 }
